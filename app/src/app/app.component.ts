6de679f3226b861dfebf70203b37144ee1451673
CYOSP <cyosp@cyosp.com>
2023-01-28 18:29:04 +0100

    Load HomeBank file and display title

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'homebank';
+  fileReader: FileReader;
+  domParser: DOMParser
+  homebankFile: Blob;
+  homebankFileLoaded: boolean;
+  homebankXmlDocument: XMLDocument;
+  title: string;
+
+  constructor() {
+    this.fileReader = new FileReader();
+    this.domParser = new DOMParser();
+    this.homebankFile = new Blob();
+    this.homebankFileLoaded = false;
+    this.homebankXmlDocument = this.domParser.parseFromString('<root/>', 'text/xml');
+    this.title = '';
+  }
+
+  homebankFileChanged(event: any) {
+    this.homebankFileLoaded = false;
+    this.homebankFile = event.target.files[0];
+    this.loadDocument();
+  }
+
+  loadDocument() {
+    this.fileReader.readAsText(this.homebankFile);
+
+    this.fileReader.onload = () => {
+      let homebankXmlFileContent = this.fileReader.result as string;
+      let startTime = new Date().getTime();
+      this.homebankXmlDocument = this.domParser.parseFromString(homebankXmlFileContent, 'text/xml');
+      console.debug('HomeBank file loaded in ' + (new Date().getTime() - startTime) + ' ms');
+      this.homebankFileLoaded = true;
+
+      this.loadTitle();
+    }
+  }
+
+  loadTitle() {
+    this.title = this.homebankXmlDocument.evaluate("/homebank/properties/@title", this.homebankXmlDocument, null, XPathResult.STRING_TYPE, null).stringValue;
+  }
 }
